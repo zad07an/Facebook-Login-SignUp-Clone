09ff4976e3ef1f67800ba81b10ce6e80576d6b78
Aram Zadoyan <aramzadoyan5@gmail.com>
2022-09-19 18:13:08 +0400

    Facebook Login and Sign Up clone

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,53 +3,30 @@ import {FaPlus} from 'react-icons/all'
 import './Footer.css'
 
 export default function Footer() {
+
+  const languages = ['English (US)','Русский','Հայերեն','Français (France)','Deutsch','العربية','Español (España)','Türkçe','Português (Brasil)','Italiano','हिन्दी']
+
+  const footerLinks = ['Sign Up','Log In','Messenger','Facebook Lite','Watch', 'Places','Games','Marketplace','Facebook Pay','Oculus','Portal','Instagram','Bulletin','Local','Fundraisers','Voting Information Center','Groups','About','Create Ad','Create Page','Developers','Careers','Privacy','Cookies','Ad choices','Terms','Help','Contact Uploading & Non-Users']
+
   return (
     <div className='footerContainer'>
       <div className="languagesContainer">
         <div className="languagesBox">
-          <a href='#' className="language">English (US)</a>
-          <a href='#' className="language">Русский</a>
-          <a href='#' className="language">Հայերեն</a>
-          <a href='#' className="language">Français (France)</a>
-          <a href='#' className="language">Deutsch</a>
-          <a href='#' className="language">العربية</a>
-          <a href='#' className="language">Español (España)</a>
-          <a href='#' className="language">Türkçe</a>
-          <a href='#' className="language">Português (Brasil)</a>
-          <a href='#' className="language">Italiano</a>
-          <a href='#' className="language">हिन्दी</a>
+          {
+            languages.map((language, idx) => {
+              return <a href='#' className="language" key={idx}>{language}</a>
+            })
+          }
           <button className='moreLanguagesButton'><FaPlus/></button>
         </div>
         <div className="footerLinksBox">
-          <li className='footerList'><a href="#" className="footerLinks">Sign Up</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Log In</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Messenger</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Facebook Lite</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Watch</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Places</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Games</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Marketplace</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Facebook Pay</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Oculus</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Portal</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Instagram</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Bulletin</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Local</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Fundraisers</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Services</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Voting Information Center</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Groups</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">About</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Create Ad</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Create Page</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Developers</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Careers</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Privacy</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Cookies</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Ad choices</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Terms</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Help</a></li>
-          <li className='footerList'><a href="#" className="footerLinks">Contact Uploading & Non-Users</a></li>
+          {
+            footerLinks.map((link, idx) => {
+              return (
+                <li key={idx} className='footerList'><a href="#" className="footerLinks">{link}</a></li>
+              )
+            })
+          }
         </div>
         <div className="metaBox">
           <span className="metaText">Meta © 2022</span>
